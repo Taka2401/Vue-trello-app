47ce74fddcae76e236792ab54906ef912e56d439
Taka2401 <tkht2401@gmail.com>
2021-10-09 01:28:58 +0900

    add: store

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,18 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    lists: []
   },
   mutations: {
+    addList(state, payload) {
+      state.lists.push({ title: payload.title, cards:[] })
+    }
   },
   actions: {
+    addList(context, payload) {
+      context.commit('addList', payload)
+    }
   },
-  modules: {
+  getters: {
   }
 })
