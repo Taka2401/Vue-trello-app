7c4d1b6ad51c940c8d7558268b96d0bd22c2ee7a
Taka2401 <tkht2401@gmail.com>
2021-10-09 01:15:30 +0900

    add: ListAdd.vue

---
+++ src/components/ListAdd.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <form class="addList" @submit.prevent="addList">
+      <input v-model="title" type="text" class="text-input" placeholder="Add new list">
+      <button type="submit" class="add-button">Add</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: ''
+    }
+  },
+  methods: {
+    addList() {
+      this.$store.dispatch('addList', {title: this.title})
+      this.title = ''
+    }
+  }
+}
+</script>
