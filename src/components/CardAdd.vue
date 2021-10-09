deec0b4f63b1f02be209b4d899eaedf82975df23
Taka2401 <tkht2401@gmail.com>
2021-10-09 13:50:19 +0900

    add: CardAdd.vue

---
+++ src/components/CardAdd.vue
@@ -0,0 +1,35 @@
+<template>
+  <form class="addcard" @submit.prevent="addCardToList">
+    <input v-model="body"
+           type="text"
+           class="text-input"
+           placeholder="Add new card"
+    />
+    <button type="submit" class="add-button">
+      Add
+    </button>
+  </form>
+</template>
+
+<script>
+export default {
+  props: {
+    listIndex: {
+      type: Number,
+      required: true,
+    }
+  },
+  data() {
+    return {
+      body: ''
+    }
+  },
+  methods: {
+    addCardToList: function() {
+      // dispatchでactionsに定義したaddCardToListを実行する
+      this.$store.dispatch('addCardToList', { body: this.body, listIndex: this.listIndex })
+      this.body = ''
+    }
+  }
+}
+</script>
