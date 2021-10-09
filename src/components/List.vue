94ab4fde8c39ceebe768a2d10bb983392d425eea
Taka2401 <tkht2401@gmail.com>
2021-10-09 13:24:30 +0900

    add: List.vue

---
+++ src/components/List.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="list">
+    <div class="listheader">
+      <p class="list-title">{{ title }}</p>
+      <div class="deletelist" @click="removeList">×</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      // 文字列指定
+      type: String,
+      // 必ず受け取る
+      required: true
+    },
+    listIndex: {
+      // 整数指定
+      type: Number,
+      // 必ず受け取る
+      required: true
+    }
+  },
+  methods: {
+    // 削除する
+    removeList: function() {
+      if(confirm('本当にこのリストを削除しますか？')){
+        // index.jsのremovelistを呼び出す
+        this.$store.dispatch('removelist', { listIndex: this.listIndex })
+      }
+    },
+  }
+}
+</script>
